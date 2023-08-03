8093d2183e370cde4f5b82b97fa9101b0d5a9ea0
Jatin-Sharma-1287 <103378007+Jatin-Sharma-1287@users.noreply.github.com>
2023-08-04 00:07:39 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://2020csb1069:12345@cluster0.n2hb0ey.mongodb.net/stack-overflow-clone"
+      "mongodb+srv://mongoyou:12345@cluster0.ls3rgmx.mongodb.net/stack-overflow-clone"
     );
     console.log("Database connected Successfully!");
   } catch (error) {
